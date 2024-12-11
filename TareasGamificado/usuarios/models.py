4870a19b209e9c061422541a4e8ff96c8435fc72
Mitchel0n <mitchel.bustamante0123@gmail.com>
2024-12-11 14:40:01 -0300

    Modificacion de crear para los usuarios y listar

--- TareasGamificado/usuarios/models.py
+++ TareasGamificado/usuarios/models.py
@@ -4,22 +4,8 @@ from django.db import models
 class Usuario(AbstractUser):
     correo = models.EmailField(unique=True)
     nivel = models.PositiveIntegerField(default=1)
-
-    # Evitar conflictos con las relaciones reverse
-    groups = models.ManyToManyField(
-        Group,
-        related_name="usuarios_groups",  # Evita conflicto con auth.User.groups
-        blank=True,
-        help_text="The groups this user belongs to.",
-        verbose_name="groups",
-    )
-    user_permissions = models.ManyToManyField(
-        Permission,
-        related_name="usuarios_permissions",  # Evita conflicto con auth.User.user_permissions
-        blank=True,
-        help_text="Specific permissions for this user.",
-        verbose_name="user permissions",
-    )
+    groups = models.ManyToManyField(Group, related_name="usuarios_groups", blank=True, help_text="The groups this user belongs to.", verbose_name="groups")
+    user_permissions = models.ManyToManyField(Permission, related_name="usuarios_permissions", blank=True, help_text="Specific permissions for this user.", verbose_name="user permissions")
 
     def __str__(self):
         return self.username
