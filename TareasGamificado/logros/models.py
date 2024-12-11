11f400dbe6ea2f39f1fd203ce5fa82deaf580e8b
Alexis <alexismhormazabal@gmail.com>
2024-12-11 00:40:55 -0300

    Agregue los models de Logro

--- TareasGamificado/logros/models.py
+++ TareasGamificado/logros/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from .models import Usuario
 # Create your models here.
+class Logro(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name="logros")
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
