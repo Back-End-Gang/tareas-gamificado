78636195af6633a93440423fb7fb0393e2056056
Mitchel Bustamante <mitchel.bustamante0123@gmail.com>
2024-12-11 02:41:50 -0300

    Agregación de tareas, solucion de problema de logros

--- TareasGamificado/tareas/models.py
+++ TareasGamificado/tareas/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from usuarios.models import Usuario  # Importa el modelo de usuario extendido
+from logros.models import Logro
 
 class Tarea(models.Model):
     ESTADOS = [('pendiente', 'Pendiente'), ('completada', 'Completada')]
@@ -14,6 +15,7 @@ class Tarea(models.Model):
         null=True,  # Permitir valores nulos
         related_name="tareas"  # Relación reverse
     )
+    logro = models.ForeignKey(Logro, null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.titulo
