e55b5d4031ad795563531bc47caeec9a55a89b72
SiekeMIS <lucasrosas.991402@gmail.com>
2024-12-11 00:43:33 -0300

    agregfue set_null

--- TareasGamificado/tareas/models.py
+++ TareasGamificado/tareas/models.py
@@ -8,5 +8,5 @@ class Tarea(models.Model):
     descripcion = models.TextField()
     estado = models.CharField(max_length=20, choices=ESTADOS, default='pendiente')
     puntos = models.PositiveIntegerField(default=0)
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name="tareas")
+    usuario = models.ForeignKey(Usuario, on_delete=models.SET_NULL, related_name="tareas")
 
