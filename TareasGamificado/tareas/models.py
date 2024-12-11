47b7bdaea300a78965201f8dfb910816cd9124e0
Mitchel Bustamante <mitchel.bustamante0123@gmail.com>
2024-12-11 00:33:12 -0300

    Modelo de aplicación Tarea, implementacion de apps logro y usuario

--- TareasGamificado/tareas/models.py
+++ TareasGamificado/tareas/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Tarea(models.Model):
+    ESTADOS = [('pendiente', 'Pendiente'), ('completada', 'Completada')]
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    estado = models.CharField(max_length=20, choices=ESTADOS, default='pendiente')
+    puntos = models.PositiveIntegerField(default=0)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name="tareas")
+
